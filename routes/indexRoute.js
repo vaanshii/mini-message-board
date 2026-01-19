06c857cb695033bce0a359a49b6a7cac2701bb3c
vaanshii <155531131+vaanshii@users.noreply.github.com>
2026-01-19 13:02:59 -0800

    Final retouch.

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -2,33 +2,52 @@ const { Router } = require("express");
 
 const indexRouter = Router();
 
+// * mock data
 const messages = [
-	{
-		text: "Hi there!",
-		user: "Amando",
-		added: new Date(),
-	},
-	{
-		text: "Hello World!",
-		user: "Charles",
-		added: new Date(),
-	},
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
 ];
 
+// * get requests
+
 indexRouter.get("/", (req, res) => {
-	res.render("index", { title: "Mini MessageBoard", messages: messages });
+  res.render("index", { title: "Mini MessageBoard", messages: messages });
+});
+
+indexRouter.get("/messages/:index", (req, res) => {
+  const messageIndex = req.params.index;
+  const message = messages[messageIndex];
+
+  if (!message) {
+    return res.status(404).send("Message not found");
+  }
+
+  res.render("viewDetails", {
+    title: `Message from ${message.user}`,
+    message: message,
+  });
 });
 
 indexRouter.get("/new", (req, res) => {
-	res.render("form", { title: "New Message" });
+  res.render("form", { title: "New Message" });
 });
 
-indexRouter.post("/new", (req, res) => {
-	const messageText = req.body.messageText;
-	const messageUser = req.body.messageUser;
+// * post requests
 
-	messages.push({ text: messageText, user: messageUser, added: new Date() });
-	res.redirect("/");
+indexRouter.post("/new", (req, res) => {
+  const messageText = req.body.messageText;
+  const messageUser = req.body.messageUser;
+
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect("/");
 });
 
 module.exports = indexRouter;
